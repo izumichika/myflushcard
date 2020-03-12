73d47af6893ee6b5705c08f94161ec11ae2a844b
izumichika <chikara0707@outlook.jp>
2020-03-12 22:26:08 +0900

    add js
    jsにカウントダウン機能文を記載

--- app/assets/javascripts/problem.js
+++ app/assets/javascripts/problem.js
@@ -1,9 +1,19 @@
-const timer = 5000  
-window.addEventListener('load',function(){
-  if(this.document.URL.match("/problems/%23")){
-    this.console.log('OK');
-    setInterval('location.reload()',timer);
-  }
-});
+// const timer = 7000  
+// window.addEventListener('load',function(){
+//   if(this.document.URL.match("/problems/%23")){
+//     this.console.log('OK');
+//     setInterval('location.reload()',timer);
+//   }
+// });
 
 
+// var count = 7;
+// var countdown = function(){
+//   document.getElementById('timer').textContent = count.toString();
+//   console.log(count--);
+//   var id = setTimeout(countdown, 7000);
+//   if(count < 0){
+//     clearTimeout(id);
+//   }
+// }
+// countdown();
