ccd073b466b942b4c26a3439062ea9981ae63fdb
izumichika <chikara0707@outlook.jp>
2020-03-15 16:13:21 +0900

    add countDowntimer
    カウントダウンタイマーを設置

--- app/assets/javascripts/problem.js
+++ app/assets/javascripts/problem.js
@@ -1,19 +1,39 @@
-// const timer = 7000  
+// var count = 7;
 // window.addEventListener('load',function(){
 //   if(this.document.URL.match("/problems/%23")){
 //     this.console.log('OK');
-//     setInterval('location.reload()',timer);
+
+//     var countdown = function(){
+//       document.getElementById('timer').textContent = count.toString();
+//       console.log(count--);
+//       var id = setTimeout(countdown, 7000);
+//       if(count < 0){
+//         clearTimeout(id);
+//       }
+//     }
+//     countdown();
+
+//     setInterval('location.reload()',count);
 //   }
 // });
 
 
-// var count = 7;
-// var countdown = function(){
-//   document.getElementById('timer').textContent = count.toString();
-//   console.log(count--);
-//   var id = setTimeout(countdown, 7000);
-//   if(count < 0){
-//     clearTimeout(id);
-//   }
-// }
-// countdown();
+$(document).ready(function(){
+  $(window).bind("load", function(){
+    if(document.URL.match("/problems/%23")){
+      $(function(){
+        cnt = 7;
+        $('#timer').text(cnt);
+        cnDown = setInterval(function(){
+          cnt--;
+          if(cnt <= 0){
+            clearInterval(cnDown);
+          }
+          $('#timer').text(cnt);
+        },1000);
+        // setInterval('location.reload()',cnt);
+      });
+      setTimeout('location.reload()',7000);
+    }
+  });
+});
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,7 @@
 class Problem < ApplicationRecord
   # belongs_to :user
   # validates :question, presence: true
+  def method_name
+  
+  end
 end
