6ff7681bcaaf33e8c59f1a8dbaa3a8104ecb9866
izumichika <chikara0707@outlook.jp>
2020-03-15 16:15:14 +0900

    delete comment out
    不要なコメントアウトを削除

--- app/assets/javascripts/problem.js
+++ app/assets/javascripts/problem.js
@@ -1,23 +1,3 @@
-// var count = 7;
-// window.addEventListener('load',function(){
-//   if(this.document.URL.match("/problems/%23")){
-//     this.console.log('OK');
-
-//     var countdown = function(){
-//       document.getElementById('timer').textContent = count.toString();
-//       console.log(count--);
-//       var id = setTimeout(countdown, 7000);
-//       if(count < 0){
-//         clearTimeout(id);
-//       }
-//     }
-//     countdown();
-
-//     setInterval('location.reload()',count);
-//   }
-// });
-
-
 $(document).ready(function(){
   $(window).bind("load", function(){
     if(document.URL.match("/problems/%23")){
